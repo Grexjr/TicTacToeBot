30bb2de2f389d123b32272baa9b3dfae84befbcc
GrexJr <geerexed@gmail.com>
2026-05-06 23:35:42 -0400

    BOTS -- HeuristicsBot starting move
    SUMMARY:
    Adds starting move heuristic to HeuristicsBot.java for early game.
    
    CHANGELOG:
    1. Adds makeStartingMove() to HeuristicsBot.java which detects the various game start cases and acts accordingly (done as a last priority)
    2. Adds isEmpty() to Board.java for ease of access to whether or not the board has been filled at all

--- src/main/java/io/github/grexjr/tictactoebot/bot/HeuristicsBot.java
+++ src/main/java/io/github/grexjr/tictactoebot/bot/HeuristicsBot.java
@@ -72,6 +72,13 @@ public class HeuristicsBot extends Bot {
         // PRIORITY 3: MAXIMIZE WINNING LANES
         maximizeWinningLines(grid,opponent.getSymbol());
 
+        // PRIORITY 4: BLOCK ENEMY WINNING LANES
+        blockEnemyWinningLines(grid,opponent.getSymbol());
+
+        // PRIORITY 5: START MOVES
+        makeStartingMove(board, opponent.getSymbol());
+
+        // ELSE: Do a random move
         if(moveIndex == -1){// Otherwise (for now) return a random value
             Random rand = new Random();
             // Random 1-9, if not blank then re rolls
@@ -80,10 +87,10 @@ public class HeuristicsBot extends Bot {
                 random = rand.nextInt(0, 9);
             }
 
-            moveIndex = random + 1;
+            moveIndex = random;
         }
 
-        return moveIndex;
+        return moveIndex + 1;
     }
 
     private boolean checkWin(char[] grid){
@@ -219,7 +226,7 @@ public class HeuristicsBot extends Bot {
             // max and change move index to this square
             if(tempWinningLines > mostWinningLines){
                 mostWinningLines = tempWinningLines;
-                moveIndex = i + 1;
+                moveIndex = i;
             }
         }
         // If you go through entire grid and find nothing, move index will be -1 still
@@ -257,6 +264,120 @@ public class HeuristicsBot extends Bot {
 
         // If both, (i == 4) return -1
         return diagonalStart;
+    }
+
+    // Method to block enemy winning lanes if they exist
+    private void blockEnemyWinningLines(char[] grid, char opponentSymbol){
+        // Set move index to blank (shouldn't matter because functions before it will change it)
+        moveIndex = -1;
+
+        // Variable for how many winning lines pass through square
+        int mostWinningLines = 0;
+
+        // Loop through all the array
+        for(int i = 0; i < grid.length; i++){
+            // Temporary variable for winning lines of specific square
+            int tempWinningLines = 0;
+
+            // If occupied, continue on
+            if(grid[i] != ' ') continue;
+
+            // Otherwise imagine what happens if you put symbol there
+
+            // Check which row & column you are in and if on diagonal
+            /* ROW CHECK:
+            Can just use modulus!
+            >if i < 3 (row 0), 0 - 0, 1 - 1, 2 - 2
+            >if i < 6 (row 3), 3 - 0, 4 - 1, 5 - 2
+            >if i < 8 (row 6), 6 - 0, 7 - 1, 8 - 2
+            */
+            int rowStart = i - (i % 3);
+            // COLUMN CHECK:
+            // Can just use modulus! since rows are +3; 0, 3, 6; 1, 4, 7; 2, 5, 8
+            int colStart = i % 3;
+            // DIAGONAL CHECK:
+            // on the evens!!!! mod 2!!!!
+            boolean isOnDiagonal = i % 2 == 0;
+
+            // Check row
+            if(checkNeighborsForEnemy(grid,opponentSymbol,rowStart,rowStart+2,1)) tempWinningLines++;
+
+            // Check column
+            if(checkNeighborsForEnemy(grid,opponentSymbol,colStart,colStart+6,3)) tempWinningLines++;
+
+            // Check diagonals if it is on the diagonal
+            if(isOnDiagonal){
+                // Do first diagonal (Ascending)
+                if(getDiagonalStartIndex(i) == -1 || getDiagonalStartIndex(i) == 0){
+                    if(checkNeighborsForEnemy(grid,opponentSymbol,0,8,4)) tempWinningLines++;
+                }
+
+                // Do descending diagonal
+                if(getDiagonalStartIndex(i) == -1 || getDiagonalStartIndex(i) == 2){
+                    if(checkNeighborsForEnemy(grid,opponentSymbol,2,6,2)) tempWinningLines++;
+                }
+            }
+
+            // Otherwise just continue on
+
+            // Then check if temp winning lines from this square are greater than current max, if so replace winning
+            // max and change move index to this square
+            if(tempWinningLines > mostWinningLines){
+                mostWinningLines = tempWinningLines;
+                moveIndex = i;
+            }
+        }
+        // If you go through entire grid and find nothing, move index will be -1 still
+    }
+
+    private boolean checkNeighborsForEnemy(char[] grid, char opponent, int start, int end, int additive){
+        int neighbors = 0;
+        int enemies = 0;
+
+        // Search through neighbors
+        // <= so we check the last index too
+        for(int i = start; i <= end; i += additive){
+            if(grid[i] == getSymbol()) neighbors++;
+            else if(grid[i] == opponent) enemies++;
+        }
+
+        // If there is one piece there, means this hypothetical would cause winning lanes
+        return neighbors == 0 && enemies == 1;
+    }
+
+    // Final heuristic: make the smartest starting move
+    // If they move first...
+        // If they take center, you take corner
+        // If they take corner, you take center
+        // If they take side, you take center
+    // If you move first...
+        // Take a corner
+    private void makeStartingMove(Board board, char opponentSymbol){
+        // array of corner indices for easy access
+        int[] corners = new int[]{0,2,6,8};
+        char[] grid = board.getRawGrid();
+
+        // If starting first (board is empty), take a corner
+        // Random for taking a random coner when needed
+        int rand = new Random().nextInt(0,4);
+
+        // If move first, take a random corner
+        if(board.isEmpty()) {
+            moveIndex = corners[rand];
+            // Early return
+            return;
+        }
+
+        // If board is not empty, check center square
+        if(grid[4] == opponentSymbol){
+            // If it equals the opponent's piece, take a corner
+            moveIndex = corners[rand];
+            // Early return
+            return;
+        }
+
+        // ELSE: If they take corner or side, take center
+        moveIndex = 4;
     }
 
 }
